e98bb685af42b9dc37d03472652d81ecc867194a
Derrick F. Mwine <47877366+derrick-mwine@users.noreply.github.com>
2019-02-23 21:15:49 +0100

    Added files to project

---
+++ js/service-register.js
@@ -0,0 +1,12 @@
+// Register Service worker
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker
+    .register('./sw.js', { scope: './' })
+    .then(function (registration) {
+      console.log("Service worker is registered");
+    })
+    .catch(function (error) {
+      console.log("Service worker is not registered", error);
+    })
+}
